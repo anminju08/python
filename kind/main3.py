cd3faf95a4a79cd63488cc899cb3395415f69078
anminju08 <anminju0310@gmail.com>
2025-07-20 23:28:46 +0900

    coding test python

---
+++ kind/main3.py
@@ -0,0 +1,16 @@
+import math
+
+MAX = 100
+prime = [True for i in range(MAX+1)]
+
+prime[1] = False
+
+for i in range(2, int(math.sqrt(MAX))+1):
+	if prime[i] == True:
+    	j = 2
+        while i*j <= MAX:
+        	prime[i*j] = False
+            j += 1
+    
+for i in range(2, number):
+    if(prime[i]): print(i)	
